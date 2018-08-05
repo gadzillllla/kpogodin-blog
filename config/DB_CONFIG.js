2e0f03ca20dbd5500da43c42f1bf76931b0a7da1
kpogodin <pogodin@restream.rt.ru>
2018-08-06 02:13:55 +0400

    init base package.json, add scss modules, ready to develop

---
+++ config/DB_CONFIG.js
@@ -0,0 +1,27 @@
+import firebase from "firebase";
+import DB_KEY from "./DB_KEY"
+
+export const DB_CONFIG = {
+  apiKey: DB_KEY,
+  authDomain: "kpogodin-blog73.firebaseapp.com",
+  databaseURL: "https://kpogodin-blog73.firebaseio.com",
+  projectId: "kpogodin-blog73",
+  storageBucket: "kpogodin-blog73.appspot.com",
+  messagingSenderId: "136260481730"
+};
+
+export const appDB = firebase.initializeApp(DB_CONFIG);
+export const database_posts = appDB
+  .database()
+  .ref()
+  .child("posts");
+
+export const database_love = appDB
+  .database()
+  .ref()
+  .child("imLove");
+
+export const database_notlove = appDB
+  .database()
+  .ref()
+  .child("notLove");
--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import './App.css';
+import Post from "./components/Post";
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         FIRST COMMIT
+        <Post />
       </div>
     );
   }
